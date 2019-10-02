11b627e655d73d1eef9d93b9d00db1c4a8b5ee7f
ptamang <adhikarimili2019@gmail.com>
2019-10-02 09:21:25 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BankApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instance of an account == object
+            var account = new Account
+            {
+                AccountEmailAddress = "testc@test.com",
+                AccountType = "Checking"
+            };
+
+            account.Deposit(2003.234m);
+
+            Console.WriteLine($"AccountNumber: {account.AccountNumber}, " +
+                $"Balance: {account.AccountBalance: C}, " +
+                $"AccountType: {account.AccountType}, " +
+                $"AccountCreateDate: {account.AccountCreateDate}, " +
+                $"accountEmailAddres: {account.AccountEmailAddress}");
+
+        }
+    }
+}
